4df9a30958ff7cb59c051db42471d3b03d8ad80d
Artem Chernenkov <temach2002@gmail.com>
2021-11-26 21:09:43 +0300

    Added loop gcd

--- GCD/GCD.cpp
+++ GCD/GCD.cpp
@@ -2,36 +2,30 @@
 #include "pch.h"
 #include <iostream>
 #include <iomanip>
-
-#include <chrono> // для функций из std::chrono
+#include <chrono>
 
 class Timer
 {
 private:
-    // Псевдонимы типов используются для удобного доступа к вложенным типам
     using clock_t = std::chrono::high_resolution_clock;
     using second_t = std::chrono::duration<double, std::ratio<1> >;
 
     std::chrono::time_point<clock_t> m_beg;
 
 public:
-    Timer() : m_beg(clock_t::now())
-    {
-    }
+    Timer() : m_beg(clock_t::now()) { }
 
-    void reset()
-    {
+    void reset() {
         m_beg = clock_t::now();
     }
 
-    double elapsed() const
-    {
+    double elapsed() const {
         return std::chrono::duration_cast<second_t>(clock_t::now() - m_beg).count();
     }
 };
 
 
-int findGCD_A(unsigned int x, unsigned int y) {
+uint32_t findGCD_A(uint32_t x, uint32_t y) {
     _asm {
         mov eax, x
         mov edx, 0
@@ -52,30 +46,52 @@ int findGCD_A(unsigned int x, unsigned int y) {
 
 
 // tail recursion, which is executed as a loop
-int findaGCD_C(int x, int y) {
-    return y ? findaGCD_C(y, x % y) : x;
+uint32_t findGCDWithTailRecursion(uint32_t x, uint32_t y) {
+    return y ? findGCDWithTailRecursion(y, x % y) : x;
+}
+
+uint32_t findGCDWithLoop(uint32_t x, uint32_t y) {
+    while (y) {
+        std::swap(x %= y, y);
+    }
+
+    return x;
 }
 
 int main()
 {
-    using namespace std;
+    uint32_t x{}, y{};
 
-    int x{}, y{};
+    std::cout << "Enter first and second number:\n";
+    while( !(std::cin >> x >> y) ) {
+        std::cout << "Enter two positive numbers again:\n";
+    }
 
-    cout << "Enter first and second number:\n";
-    cin >> x >> y;
+    Timer time;
 
-    Timer t;
-    int amount_a = findGCD_A(x, y);
-    auto time_a = t.elapsed();
-    t.reset();
+    uint32_t amountAssembler = findGCD_A(x, y);
+    auto timeWithAssembler= time.elapsed();
+    time.reset();
 
-    int amount_c = findaGCD_C(x, y);
-    auto time_c = t.elapsed();
+    uint32_t amountWithTailRecursion = findGCDWithTailRecursion(x, y);
+    auto timeWithTailRecursion = time.elapsed();
+    time.reset();
 
-    std::cout << fixed << setprecision(9) << "\nAnswer(assembler):  " << amount_a << '\n' <<
-        "Anser(C++):  " << amount_c << "\n\n" <<
-        "Time(assembler):  " << time_a << '\n' <<
-        "Time(C++):  " << time_c;
+    uint32_t amountWithLoop = findGCDWithLoop(x, y);
+    auto timeWithLoop = time.elapsed();
+
+
+    std::cout.precision(9);
+    std::cout.setf(std::ios::fixed, std::ios::floatfield);
+
+    std::cout << '\n';
+    std::cout << "Answer(assembler):\t\t"           << amountAssembler          << '\n';
+    std::cout << "Anser(tail recursion, C++):\t"    << amountWithTailRecursion  << '\n';
+    std::cout << "Anser(loop, C++):\t\t"            << amountWithLoop           << "\n\n";
+
+    std::cout << "Time(assembler):\t\t"             << timeWithAssembler        << '\n';
+    std::cout << "Time(tail recursion, C++):\t"     << timeWithTailRecursion    << '\n';
+    std::cout << "Time(loop, C++):\t\t"             << timeWithLoop             << '\n';
+
     return 0;
 }
